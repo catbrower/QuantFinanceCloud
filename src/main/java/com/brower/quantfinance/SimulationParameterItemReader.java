8dae27d5b7aaf0dc0a625bb133b456a6d148fd06
Catherine Brower <catherinemb42@gmail.com>
2023-03-18 01:44:44 -0400

    inital commit

---
+++ src/main/java/com/brower/quantfinance/SimulationParameterItemReader.java
@@ -0,0 +1,7 @@
+package com.brower.quantfinance;
+
+/***
+ * This class is responsible for returning parameters used to perform a backtesting simulation
+ */
+public class SimulationParameterItemReader {
+}
